25c337a7a79cf72d28ac941964a3381192acdd12
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-10-11 19:36:13 +0530

    update packge-lock.json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express, {Application} from 'express';
 import userRoutes from './routes/userRouts';
 import itemRouts from './routes/itemRouts';
+import authRouts from './routes/authRouts';
 import mongoose from "mongoose"
+import path from 'path';
+import dotenv from "dotenv";
+
+
+// load .env located in src/ (if you keep .env in src)
+dotenv.config({ path: path.join(__dirname, '.env') });
+
 
 const App: Application = express();
 App.use(express.json());
 App.use('/api/v1/user', userRoutes);
 App.use('/api/v1/item', itemRouts);
+App.use('/api/v1/auth', authRouts);
 
 const mongo  =  mongoose.connect('mongodb://localhost:27017/userDB')
 .then(() => {
